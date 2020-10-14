4de89bf6e47a9b68ee22d9fb8e86496c455dfd78
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2020-10-14 13:19:28 -0300

    Add possibility to validate API fields

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -3,6 +3,7 @@
 /* eslint-disable camelcase */
 import { Request, Response } from "express";
 import { getRepository } from "typeorm";
+import * as Yup from "yup";
 import Orphanage from "../models/Orphanage";
 import orphanageView from "../views/orphanages_view";
 
@@ -46,7 +47,7 @@ export default {
     const requestImages = request.files as Express.Multer.File[];
     const images = requestImages.map((image) => ({ path: image.filename }));
 
-    const orphanage = orphanageRepository.create({
+    const data = {
       name,
       latitude,
       longitude,
@@ -55,7 +56,28 @@ export default {
       opening_hours,
       open_on_weekends,
       images,
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required(),
+        })
+      ),
     });
+
+    await schema.validate(data, {
+      abortEarly: false, // retorna todos os erros de todos os campos que tiverem
+    });
+
+    const orphanage = orphanageRepository.create(data);
 
     await orphanageRepository.save(orphanage);
 
