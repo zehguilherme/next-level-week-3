9f6f28ea851ee8970f097076ec1e7f9e7d6b4d71
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2020-10-14 16:26:53 -0300

    Insert the SideBar component inside each page; Removing the previous code that represented this component

--- web/src/pages/CreateOrphanage.tsx
+++ web/src/pages/CreateOrphanage.tsx
@@ -1,70 +1,70 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
 import React from "react";
-import { Map, Marker, TileLayer } from 'react-leaflet';
-import L from 'leaflet';
-import { useHistory } from "react-router-dom";
+import { Map, Marker, TileLayer } from "react-leaflet";
+import L from "leaflet";
+import { FiPlus } from "react-icons/fi";
 
-import { FiArrowLeft, FiPlus } from "react-icons/fi";
+import mapMarkerImg from "../images/map-marker.svg";
+import Sidebar from "../components/Sidebar";
 
-import mapMarkerImg from '../images/map-marker.svg';
-
-import '../styles/pages/create-orphanage.css';
+import "../styles/pages/create-orphanage.css";
 
 const happyMapIcon = L.icon({
   iconUrl: mapMarkerImg,
 
   iconSize: [58, 68],
+
   iconAnchor: [29, 68],
-  popupAnchor: [0, -60]
-})
+
+  popupAnchor: [0, -60],
+});
 
 export default function CreateOrphanage() {
-  const { goBack } = useHistory();
-
   return (
     <div id="page-create-orphanage">
-      <aside>
-        <img src={mapMarkerImg} alt="Happy" />
-
-        <footer>
-          <button type="button" onClick={goBack}>
-            <FiArrowLeft size={24} color="#FFF" />
-          </button>
-        </footer>
-      </aside>
+      <Sidebar />
 
       <main>
         <form className="create-orphanage-form">
           <fieldset>
             <legend>Dados</legend>
 
-            <Map 
-              center={[-27.2092052,-49.6401092]} 
-              style={{ width: '100%', height: 280 }}
+            <Map
+              center={[-27.2092052, -49.6401092]}
+              style={{ width: "100%", height: 280 }}
               zoom={15}
             >
-              <TileLayer 
+              <TileLayer
                 url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
               />
 
-              <Marker interactive={false} icon={happyMapIcon} position={[-27.2092052,-49.6401092]} />
+              <Marker
+                interactive={false}
+                icon={happyMapIcon}
+                position={[-27.2092052, -49.6401092]}
+              />
             </Map>
 
             <div className="input-block">
               <label htmlFor="name">Nome</label>
+
               <input id="name" />
             </div>
 
             <div className="input-block">
-              <label htmlFor="about">Sobre <span>Máximo de 300 caracteres</span></label>
+              <label htmlFor="about">
+                Sobre
+                <span>Máximo de 300 caracteres</span>
+              </label>
+
               <textarea id="name" maxLength={300} />
             </div>
 
             <div className="input-block">
               <label htmlFor="images">Fotos</label>
 
-              <div className="uploaded-image">
-
-              </div>
+              <div className="uploaded-image" />
 
               <button className="new-image">
                 <FiPlus size={24} color="#15b6d6" />
@@ -77,11 +77,13 @@ export default function CreateOrphanage() {
 
             <div className="input-block">
               <label htmlFor="instructions">Instruções</label>
+
               <textarea id="instructions" />
             </div>
 
             <div className="input-block">
               <label htmlFor="opening_hours">Nome</label>
+
               <input id="opening_hours" />
             </div>
 
@@ -89,7 +91,10 @@ export default function CreateOrphanage() {
               <label htmlFor="open_on_weekends">Atende fim de semana</label>
 
               <div className="button-select">
-                <button type="button" className="active">Sim</button>
+                <button type="button" className="active">
+                  Sim
+                </button>
+
                 <button type="button">Não</button>
               </div>
             </div>
