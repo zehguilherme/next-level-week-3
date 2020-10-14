377c8a565d1b722192bb5442c1fc46dfb77aa906
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2020-10-14 11:35:53 -0300

    Add OrphanagesController

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,64 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+/* eslint-disable camelcase */
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import Orphanage from "../models/Orphanage";
+import orphanageView from "../views/orphanages_view";
+
+export default {
+  async index(request: Request, response: Response) {
+    const orphanageRepository = getRepository(Orphanage);
+
+    const orphanages = await orphanageRepository.find({
+      relations: ["images"],
+    });
+
+    return response.json(orphanageView.renderMany(orphanages));
+  },
+
+  async show(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const orphanageRepository = getRepository(Orphanage);
+
+    const orphanage = await orphanageRepository.findOneOrFail(id, {
+      relations: ["images"],
+    });
+
+    return response.json(orphanageView.render(orphanage));
+  },
+
+  async create(request: Request, response: Response) {
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    } = request.body;
+
+    const orphanageRepository = getRepository(Orphanage);
+
+    //                                     array de arquivos do Multer
+    const requestImages = request.files as Express.Multer.File[];
+    const images = requestImages.map((image) => ({ path: image.filename }));
+
+    const orphanage = orphanageRepository.create({
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+      images,
+    });
+
+    await orphanageRepository.save(orphanage);
+
+    return response.status(201).json(orphanage);
+  },
+};
