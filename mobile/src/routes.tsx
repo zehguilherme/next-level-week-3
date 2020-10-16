11f74aaa74b3dcae5d67c9609739271ecad85eac
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2020-10-16 11:24:31 -0300

    Add routes file with routes OrphanagesMap and OrphanageDetails

---
+++ mobile/src/routes.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+import React from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import OrphanagesMap from "./pages/OrphanagesMap";
+import OrphanageDetails from "./pages/OrphanageDetails";
+
+const { Navigator, Screen } = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Navigator screenOptions={{ headerShown: false }}>
+        <Screen name="OrphanagesMap" component={OrphanagesMap} />
+        <Screen name="OrphanageDetails" component={OrphanageDetails} />
+      </Navigator>
+    </NavigationContainer>
+  );
+}
