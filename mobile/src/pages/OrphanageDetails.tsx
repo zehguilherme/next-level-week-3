6f2f382449d9f697504e9003cbf94d96d8f7961b
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2020-10-16 18:32:34 -0300

    Create a conditional based on a value coming from the API; Add new styles based on her

--- mobile/src/pages/OrphanageDetails.tsx
+++ mobile/src/pages/OrphanageDetails.tsx
@@ -123,13 +123,23 @@ export default function OrphanageDetails() {
             </Text>
           </View>
 
-          <View style={[styles.scheduleItem, styles.scheduleItemGreen]}>
-            <Feather name="info" size={40} color="#39CC83" />
+          {orphanage.open_on_weekends ? (
+            <View style={[styles.scheduleItem, styles.scheduleItemGreen]}>
+              <Feather name="info" size={40} color="#39CC83" />
 
-            <Text style={[styles.scheduleText, styles.scheduleTextGreen]}>
-              Atendemos fim de semana
-            </Text>
-          </View>
+              <Text style={[styles.scheduleText, styles.scheduleTextGreen]}>
+                Atendemos fim de semana
+              </Text>
+            </View>
+          ) : (
+            <View style={[styles.scheduleItem, styles.scheduleItemRed]}>
+              <Feather name="info" size={40} color="#FF669D" />
+
+              <Text style={[styles.scheduleText, styles.scheduleTextRed]}>
+                Não atendemos fim de semana
+              </Text>
+            </View>
+          )}
         </View>
 
         <RectButton style={styles.contactButton} onPress={() => {}}>
@@ -231,6 +241,13 @@ const styles = StyleSheet.create({
     borderRadius: 20,
   },
 
+  scheduleItemRed: {
+    backgroundColor: "#FEF6F9",
+    borderWidth: 1,
+    borderColor: "#FFBCD4",
+    borderRadius: 20,
+  },
+
   scheduleText: {
     fontFamily: "Nunito_600SemiBold",
     fontSize: 16,
@@ -244,6 +261,10 @@ const styles = StyleSheet.create({
 
   scheduleTextGreen: {
     color: "#37C77F",
+  },
+
+  scheduleTextRed: {
+    color: "#FF669D",
   },
 
   contactButton: {
